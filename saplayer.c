01f0737bd6dd3d8cea4d0708427a6951ad22314f
Chenguang Wang <w3cing@gmail.com>
2011-07-18 03:49:31 +0800

    modified the LGPL description header in saplayer.c

--- saplayer.c
+++ saplayer.c
@@ -2,20 +2,20 @@
  * saplayer.c
  * Copyright (C) 2011 by Chenguang Wang(wecing)
  *
- * This file is part of SimpleAV.
+ * This file is part of SimpleAV-SDL.
  *
- * SimpleAV is free software: you can redistribute it and/or modify
+ * SimpleAV-SDL is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * SimpleAV is distributed in the hope that it will be useful,
+ * SimpleAV-SDL is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with SimpleAV. If not, see <http://www.gnu.org/licenses/>.
+ * along with SimpleAV-SDL. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <SDL/SDL.h>
